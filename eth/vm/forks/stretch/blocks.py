a3472243301e8c19cdfb06b4fef401a78843368c
barnabemonnot <barnabe.monnot@yahoo.fr>
2018-12-08 22:06:24 +0800

    Add xmessages to block

--- eth/vm/forks/stretch/blocks.py
+++ eth/vm/forks/stretch/blocks.py
@@ -12,10 +12,19 @@ from eth.vm.forks.byzantium.transactions import (
     ByzantiumTransaction,
 )
 
+from eth.vm.forks.stretch.xmessage import {
+    StretchXMessage,
+    StretchXMessageReceived
+}
+
 class StretchBlock(ByzantiumBlock):
     transaction_class = ByzantiumTransaction
+    xmessage_sent_class = StretchXMessage
+    xmessage_received_class = StretchXMessageReceived
     fields = [
         ('header', BlockHeader),
         ('transactions', CountableList(transaction_class)),
+        ('xmessages_sent', CountableList(xmessage_sent_class)),
+        ('xmessages_received', CountableList(xmessage_received_class)),
         ('uncles', CountableList(BlockHeader))
     ]
