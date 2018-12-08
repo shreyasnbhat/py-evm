6f834aab0b10319dce9da8fb5f534ce0f45fc5fc
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2018-12-08 21:24:59 +0800

    Added XMessage classes

---
+++ eth/rlp/xmessages.py
@@ -0,0 +1,105 @@
+from abc import (
+    ABC,
+    abstractmethod
+)
+
+import rlp
+from rlp.sedes import (
+    big_endian_int,
+    binary,
+)
+
+from eth_typing import (
+    Address
+)
+
+from eth_hash.auto import keccak
+from eth_keys.datatypes import (
+    PrivateKey
+)
+from eth_utils import (
+    ValidationError,
+)
+
+from eth.rlp.sedes import (
+    address,
+    hash32
+)
+
+from eth.vm.computation import (
+    BaseComputation
+)
+
+
+class BaseXMessageMethods:
+    def validate(self) -> None:
+        pass
+
+
+class BaseXMessageFields(rlp.Serializable):
+
+    fields = [
+        ('gas_price', big_endian_int),
+        ('gas', big_endian_int),
+        ('to', address),
+        ('value', big_endian_int),
+        ('data', binary),
+        ('sender', address),
+        ('shard_id', big_endian_int),
+        ('base', hash32)
+    ]
+
+    @property
+    def hash(self) -> bytes:
+        return keccak(rlp.encode(self))
+
+
+class BaseXMessage(BaseXMessageFields, BaseXMessageMethods):
+    @classmethod
+    def from_base_xmessage(cls, xmessage: 'BaseXMessage') -> 'BaseXMessage':
+        return rlp.decode(rlp.encode(transaction), sedes=cls)
+
+    @property
+    def sender(self) -> Address:
+        """
+        Convenience property for the return value of `get_sender`
+        """
+        return self.get_sender()
+
+    # +-------------------------------------------------------------+
+    # | API that must be implemented by all Transaction subclasses. |
+    # +-------------------------------------------------------------+
+
+    #
+    # Validation
+    #
+    def validate(self) -> None:
+        """
+        Checks xmessage validity, raising a ValidationError if the xmessage
+        is invalid.
+        """
+        raise NotImplementedError("Must be implemented by subclasses")
+
+
+    @abstractmethod
+    def get_sender(self) -> Address:
+        """
+        Get the 20-byte address which sent this xmessage.
+        """
+        return self.sender
+
+    @classmethod
+    def from_transaction(cls, tx, shard_id, base):
+        xmessage_dict = tx.copy()
+        xmessage_dict['sender'] = tx.sender
+        xmessage_dict['shard_id'] = tx.shard_id
+        xmessage_dict['base'] = tx.base
+
+    def get_transaction_dict(self):
+        return {
+            'gas_price': self.gas_price,
+            'gas': self.gas,
+            'to': self.to,
+            'value': self.value,
+            'data': self.data
+        }
---
+++ eth/vm/forks/stretch/xmessage.py
@@ -0,0 +1,46 @@
+from rlp.sedes import (
+    big_endian_int,
+    binary,
+)
+from eth.rlp.sedes import (
+    address,
+    hash32
+)
+
+from eth.rlp.xmessage import (
+    BaseXMessage,
+)
+
+class StretchXMessage(BaseXMessage):
+    def validate(self):
+        assert False, "Yet to implement"
+
+class StretchXMessageReceived(StretchXMessage):
+    fields = [
+        ('gas_price', big_endian_int),
+        ('gas', big_endian_int),
+        ('to', address),
+        ('value', big_endian_int),
+        ('data', binary),
+        ('sender', address),
+        ('shard_id', big_endian_int),
+        ('base', hash32),
+        ('source_shard_id', big_endian_int),
+        ('source', hash32),
+    ]
+
+    @classmethod
+    def from_transaction(cls, tx, shard_id, base, source_shard_id, source):
+        xmessage_dict = {
+            'gas_price': tx.gas_price,
+            'gas': tx.gas,
+            'to': tx.to,
+            'value': tx.value,
+            'data': tx.data,
+            'sender': tx.sender,
+            'shard_id': shard_id,
+            'base': base,
+            'source_shard_id': source_shard_id,
+            'source': source
+        }
+        return cls(**xmessage_dict)
